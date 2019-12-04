37c90f379350ca5411737cd8415a8c597d95f0a4
Xiaolei Wang <xwang@sandiego.edu>
2019-12-04 02:09:25 -0800

    updated landing page text

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -11,9 +11,9 @@ class Landing extends Component {
                         <h2>Sharing chores we don't want to do, together!</h2>
                     </header>
                     <p>
-                        Chore Share will let roomies share chores in one place! Click "View Chores" to get started. Next, click "Add a Roomie" to add you and your roommate(s). Then click "Add a Chore" and select a roomie to start keeping track of each other's chores.
+                        Chore Share will let roomies share chores in one place! Click "Together!" to get started. Next, click "Add a Roomie" to add you and your roommate(s). Then click "Add a Chore" and select a roomie to start keeping track of each other's chores.
                     </p>
-                    <button type="button"><Link to='/home'>View Chores</Link></button>
+                    <button type="button"><Link to='/home'>Together!</Link></button>
                 </section>
             </main>
         );
